4b2f02a2d3fc69e2674ef3db2b3ada45b488cc9f
BUNNY <darkydaddy77@gmail.com>
2023-10-12 00:46:04 +0530

    j

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheChampu/CruellaV2.0",
+    "https://github.com/rameshkumar656/kuno-2.0.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
